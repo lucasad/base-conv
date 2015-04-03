16eadca0bd318668654a1d0e7f6b466468a024d4
Lucas Dohring <lucas+github@dohring.tk>
2015-04-02 23:03:41 -0700

    Added pseudo-readline header and header guards

--- base-conv.h
+++ base-conv.h
@@ -1,5 +1,9 @@
+#ifndef BASE_CONV_H
+#define BASE_CANV_H
+
 int parse_number(const char *string, unsigned char base, unsigned *number); 
 char* serialize_number(unsigned int number, unsigned char base, char *buf); 
 
 char char_to_digit(char chr); 
 char digit_to_char(char digit); 
+#endif /* BASE_CONV_H */
